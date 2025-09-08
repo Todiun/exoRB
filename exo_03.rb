a619b06c00730ff32d9c1e4cbd9fc0df7ef7c396
root <root@DESKTOP-JOSEPH.localdomain>
2025-09-08 11:24:31 +0200

    fixed

---
+++ exo_03.rb
@@ -0,0 +1,8 @@
+#my_third_program.rb
+puts "Quel age as-tu ?"
+
+age = gets
+agethen = age.to_i - 8 
+puts "En 2017 tu avais"
+puts agethen  
+puts "ans"
---
+++ exo_04.rb
@@ -0,0 +1,5 @@
+#my_fourth_program.rb
+puts "Année de naissance ?"
+annee = gets
+puts "Tu auras 100 ans en"
+puts annee.to_i + 100
