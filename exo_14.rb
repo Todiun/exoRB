26fe948b6843305c31a4ca5a11eaea86be2a3631
root <root@DESKTOP-JOSEPH.localdomain>
2025-09-09 12:44:24 +0200

    exo 14 fini

--- exo_14.rb
+++ exo_14.rb
@@ -1,13 +1,15 @@
 
 mails = Array.new
 adress = "jean.dupont"
-number = 0
+number = 01
 mail = "@email.fr"
 while number <= 50
-    print adress
-    print number
-    puts mail
-    number +=1
-    mails [0]
+    complete =adress + number.to_s + mail
+    mails.push (complete) 
+    number +=1 
 end
-    print mails []
+auto = 1
+while auto <= 50
+print mails[auto]
+auto +=2
+end
