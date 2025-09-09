1007fcd338a1594adebe2401699f61ef0e032b03
root <root@DESKTOP-JOSEPH.localdomain>
2025-09-09 12:16:09 +0200

    exo 14 en cours

---
+++ exo_14.rb
@@ -0,0 +1,13 @@
+
+mails = Array.new
+adress = "jean.dupont"
+number = 0
+mail = "@email.fr"
+while number <= 50
+    print adress
+    print number
+    puts mail
+    number +=1
+    mails [0]
+end
+    print mails []
